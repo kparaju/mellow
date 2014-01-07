272545cb0c6a936718d04c0e17bd8c28da9b8fb0
Kshitij Parajuli <kshitijparajuli@gmail.com>
2014-01-07 11:00:53 -0500

    Images and gifs are displayed instead of links

--- mellow.js
+++ mellow.js
@@ -11,8 +11,18 @@ if (Meteor.isClient) {
     }
   });
 
+  var testRegex = /^https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png)$/;
+
   Template.room.messages = function() {
-    return Room.find();
+    var messages = Room.find().fetch();
+    var fixedMessages = [];
+    messages.forEach(function(element) {
+      if (testRegex.test(element.message)) {
+        element.message="<img src='" + element.message + "' />"
+      }
+      fixedMessages.push(element);
+    });
+    return fixedMessages;
   }
 }
 
