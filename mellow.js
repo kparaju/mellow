097c795bdfbca1ff10ab9ecb1710d52cf68b0130
Kshitij Parajuli <kshitijparajuli@gmail.com>
2014-01-15 19:33:11 -0500

    Autoscroll to end of chat

--- mellow.js
+++ mellow.js
@@ -11,19 +11,26 @@ if (Meteor.isClient) {
     }
   });
 
-  var testRegex = /^https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png)$/;
+  var imgTestRegex = /^https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png)$/;
+  
 
   Template.room.messages = function() {
     var messages = Room.find().fetch();
     var fixedMessages = [];
     messages.forEach(function(element) {
-      if (testRegex.test(element.message)) {
+      if (imgTestRegex.test(element.message)) {
         element.message="<img src='" + element.message + "' />"
       }
       fixedMessages.push(element);
     });
     return fixedMessages;
   }
+  
+  Template.room.rendered = function () {
+    $(".chat-logs-container").scrollTop($(".chat-logs-container")[0].scrollHeight);
+  }
+  
+  
 }
 
 if (Meteor.isServer) {
